9ce1c687f8b3e6115872edfbc0b43fdacd06ea42
HSkuza <hubert.skuza@execon.pl>
2022-02-07 19:05:24 +0100

    dependency injection without spring

---
+++ src/main/java/com/hubert/pcdi/controller/PropertyInjectedController.java
@@ -0,0 +1,11 @@
+package com.hubert.pcdi.controller;
+
+import com.hubert.pcdi.services.GreetingService;
+
+public class PropertyInjectedController {
+  public GreetingService greetingService;
+
+  public String getGreeting() {
+    return greetingService.sayGreeting();
+  }
+}
---
+++ src/main/java/com/hubert/pcdi/services/GreetingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.hubert.pcdi.services;
+
+public class GreetingServiceImpl implements GreetingService {
+  @Override
+  public String sayGreeting() {
+    return "HELLO WORLD!!!!!";
+  }
+}
